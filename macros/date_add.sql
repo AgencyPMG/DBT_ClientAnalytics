bf6e639badbd53a896572e4dd8879484bf6b1529
Mumtaz Bano <86626735+mumtazbano1@users.noreply.github.com>
2022-02-10 16:24:25 -0600

    Delete date_add.sql

--- macros/date_add.sql
+++
@@ -1,7 +0,0 @@
-{% macro date_add(date,interval) %}
-    {% if target.type == 'bigquery' %}
-        DATE_ADD({{date}}, interval {{interval}} day)
-    {% elif target.type == 'redshift' %}
-        dateadd(day,{{interval}},{{date}})
-    {% endif %}
-{% endmacro %}
